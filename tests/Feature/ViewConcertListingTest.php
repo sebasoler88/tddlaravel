11d301cefd232599e1b8f64cb73ee648dc291805
Sebastian Soler <sebastian.s@geopagos.com>
2019-09-18 23:36:19 -0300

    Initial commit - Concert listing

---
+++ tests/Feature/ViewConcertListingTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Concert;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Carbon;
+use Tests\TestCase;
+
+class ViewConcertListingTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function user_can_view_a_concert_listing()
+    {
+        $concert = Concert::create([
+            'title' => 'the Red Chord',
+            'subtitle' => 'with Animosity and Lethargy',
+            'date' => Carbon::parse('December 13, 2016 8:00pm'),
+            'ticket_price' => 3250,
+            'venue' => 'The Mosh Pit',
+            'venue_address' => '123 Example Lane',
+            'city' => 'Laraville',
+            'state' => 'ON',
+            'zip' => '17916',
+            'additional_information' => 'For tickets, call (555) 555-5555.',
+        ]);
+
+        $response = $this->get('/concerts/' . $concert->id);
+
+        $response->assertSee('the Red Chord');
+        $response->assertSee('with Animosity and Lethargy');
+        $response->assertSee('December 13, 2016');
+        $response->assertSee('8:00pm');
+        $response->assertSee(32.50);
+        $response->assertSee('The Mosh Pit');
+        $response->assertSee('123 Example Lane');
+        $response->assertSee('Laraville');
+        $response->assertSee('ON');
+        $response->assertSee('17916');
+        $response->assertSee('For tickets, call (555) 555-5555.');
+    }
+}
